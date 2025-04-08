79d8acd1bf11067ea1c213772234701a76f6c6b8
delform17 <damian.clear@gmail.com>
2025-04-08 16:46:27 +0200

    Update countReference_And_ControlValue.jsx
    Update with new styles

--- countReference_And_ControlValue.jsx
+++ countReference_And_ControlValue.jsx
@@ -2,18 +2,28 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { useRef } from 'react';
 
- //Jesli wartosc referencji bedzie parzysta czysc referencje 
-//Po kazdym kliknieciu podwajamy wartosc  referencji jesli  osiagnie pozadana 
-//wartosc  czyscimy referencje 
-
 const referenceVal = 4;
 
 function ReferenceCount_And_ValControl(){
 
+    const txtStyles = {
+
+        txtStyle : {
+            fontSize: '14px',
+            fontFamily : 'Courier',
+            fontStyle : 'regular',
+            color : 'white'
+        }
+    }
+
+    const [initTextState, changeTextState] = useState('Inicjuj status');
+    const [actualTextState, changeTS] = useState('Present value of Reference : ');
+    const [goalTextState, changeGoalTS] = useState('');
    
     useEffect(() => {
         const clock = setTimeout(() => {
-            console.log('Inicjuj status');
+            //console.log('Inicjuj status');
+
         },2000);
     },[]);
 
@@ -23,15 +33,19 @@ function ReferenceCount_And_ValControl(){
     const printTheMessage = (v) => {
         v.preventDefault();
         clockRef.current = clockRef.current * 2;
-        console.log(clockRef.current);
+        //console.log(clockRef.current);
+        changeGoalTS(clockRef.current);
         
         const x =  setTimeout(() => {
-            console.log('Present value of Reference : '+clockRef.current);
+            //console.log('Present value of Reference : '+clockRef.current);
         },2000);
 
         if(clockRef.current > 6)
         {
-            console.log('We have  reached max value of referrence');
+            //let component = document.getElementById("textComponent");
+
+            //console.log('We have  reached max value of referrence');
+            changeGoalTS('We have  reached max value of referrence');
            
             clearTimeout(x);
             
@@ -41,7 +55,9 @@ function ReferenceCount_And_ValControl(){
    return(
         <div>
             <button onClick={printTheMessage}>Increase</button>
-
+            <span id="textComponent" style={txtStyles.txtStyle}>{initTextState}</span><br></br>
+            <span style={txtStyles.txtStyle}>{actualTextState+clockRef.current}</span>
+            <p style={txtStyles.txtStyle}>{goalTextState}</p>
         </div>
     )
 }
